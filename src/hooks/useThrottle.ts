8fb78f4b30ca500eba864a9a87971895276d55a4
Mehdiradx <mehdiradx@gmail.com>
2026-01-04 22:53:59 +0330

    feat(utils): add debounce and throttle utilities

---
+++ src/hooks/useThrottle.ts
@@ -0,0 +1,11 @@
+export function throttle<T extends (...args: any[]) => void>(fn: T, limit: number) {
+  let inThrottle = false;
+
+  return (...args: Parameters<T>) => {
+    if (!inThrottle) {
+      fn(...args);
+      inThrottle = true;
+      setTimeout(() => (inThrottle = false), limit);
+    }
+  };
+}
